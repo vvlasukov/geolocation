5abfc0b03a5f9ad1323b46655176d5b4fd8e5fc5
vvlasukov <vvlasukov@avito.ru>
2020-08-31 15:54:38 +0300

    first commit

---
+++ force-app/main/default/aura/AccountSearch/AccountSearchController.js
@@ -0,0 +1,13 @@
+({
+  onInit: function (component, event, helper) {
+    // proactively search on component initialization
+    var searchTerm = component.get("v.searchTerm");
+    helper.handleSearch(component, searchTerm);
+  },
+
+  onSearchTermChange: function (component, event, helper) {
+    // search anytime the term changes in the input field
+    var searchTerm = component.get("v.searchTerm");
+    helper.handleSearch(component, searchTerm);
+  }
+});
